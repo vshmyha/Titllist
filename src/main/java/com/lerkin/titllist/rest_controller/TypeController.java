8992ad8039724eb68996146ad11c05671c4ba6e3
vshmyha <valeria.shmyha@gmail.com>
2021-09-26 16:20:40 +0300

    enum of types instead TypeEntity

--- src/main/java/com/lerkin/titllist/rest_controller/TypeController.java
+++ src/main/java/com/lerkin/titllist/rest_controller/TypeController.java
@@ -1,26 +1,25 @@
 package com.lerkin.titllist.rest_controller;
 
-import com.lerkin.titllist.dao.entity_db.TypeEntity;
-import com.lerkin.titllist.service.type.TypeService;
+import com.lerkin.titllist.dao.entity_db.Type;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping(Navigation.TYPE)
 @RestController
 @RequiredArgsConstructor
 public class TypeController {
 
-	private final TypeService typeService;
-
 	@GetMapping
-	public ResponseEntity<List<TypeEntity>> getTypes() {
+	public ResponseEntity<List<String>> getTypes() {
 
-		List<TypeEntity> types = typeService.getTypes();
+		List<String> types = Arrays.stream(Type.values()).map(Type::getTypeName).collect(Collectors.toList());
 		return ResponseEntity.ok(types);
 	}
 }
